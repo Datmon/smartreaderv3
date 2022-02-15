3b77bb834dda53514905edb05e46313046c2071d
Данил Анпилов <danilanpilov@MacBook-Air-Danil.local>
2022-02-15 15:55:44 +0600

    remove pdf reader

--- src/screens/Bookshelf/Bookshelf.tsx
+++ src/screens/Bookshelf/Bookshelf.tsx
@@ -77,51 +77,6 @@ const Bookshelf = ({
     modalizeRef.current?.close();
   };
 
-  async function readPdfMetadata() {
-    // Fetch PDF
-    const pdfUrl = 'https://pdf-lib.js.org/assets/with_cropbox.pdf';
-    const pdfBytes = await axios
-      .get(pdfUrl, {
-        responseType: 'arraybuffer',
-        headers: {
-          Accept: 'application/pdf',
-        },
-      })
-      .then(res => {
-        console.log('resPDF', res);
-        return res.data;
-      });
-
-    // Load the PDF document without updating its existing metadata
-    const pdfDoc = await PDFDocument.load(pdfBytes, {
-      updateMetadata: false,
-    });
-
-    console.log('pdfDoc', pdfDoc);
-
-    // Read all available metadata fields
-    const pdftitle = pdfDoc.getTitle();
-    const author = pdfDoc.getAuthor();
-    const subject = pdfDoc.getSubject();
-    const creator = pdfDoc.getCreator();
-    const keywords = pdfDoc.getKeywords();
-    const producer = pdfDoc.getProducer();
-    const creationDate = pdfDoc.getCreationDate();
-    const modificationDate = pdfDoc.getModificationDate();
-
-    console.log('author', author);
-
-    const file = require('../../assets/books/9019827.a4.pdf');
-
-    // const blob = new PolyfillBlob(
-    //   file,
-    //   {
-    //     type: 'application/pdf',
-    //   },
-    //   true,
-    // );
-  }
-
   const handleError = (err: unknown) => {
     if (DocumentPicker.isCancel(err)) {
       console.warn('cancelled');
@@ -136,7 +91,6 @@ const Bookshelf = ({
   };
 
   useEffect(() => {
-    readPdfMetadata();
     setFiltredBooks(filtedBooks);
   }, []);
 
